46f78dd859fb35e7a8b9afa2fafebfd2bcc7267e
jared <hamjared5@gmail.com>
2020-10-06 23:17:33 +0100

    swithcing to evdev

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 import pyglet
+import evdev
 
 # 0 axis = aileron
 # 1 axis = elevator
@@ -8,16 +9,11 @@ import pyglet
 # 3 axis = rudder
 from Joystick import Joystick
 
-def poll_joystick(dt):
-    print(joystick.x)
 
 
 if __name__ == '__main__':
-
-    joystick = pyglet.input.get_joysticks()[0]
-    joystick.open()
-    pyglet.clock.schedule_interval(poll_joystick, 0.1)
-    pyglet.app.run()
-
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    for device in devices:
+        print( device.name)
 
 
