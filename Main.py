dd975cc4983311f0a3251d49fdc1fe3f3a8f9f3c
Jared <jaredlham@gmail.com>
2020-10-06 19:49:20 -0600

    change

--- Main.py
+++ Main.py
@@ -15,5 +15,8 @@ if __name__ == '__main__':
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
     joystick = evdev.InputDevice(devices[0].path)
     print (joystick)
+    while True:
+        for event in joystick.read_loop():
+            print(event)
 
 
